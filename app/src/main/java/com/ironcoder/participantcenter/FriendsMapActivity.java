28e05ff26e582017a1c0a1c403a5a3f765113d5a
KYLE MARTINEZ <kylemartinez@KYLEs-MacBook-Pro.local>
2014-07-24 16:05:36 -0500

    Trying to connect to the google server. so far no luck

--- app/src/main/java/com/ironcoder/participantcenter/FriendsMapActivity.java
+++ app/src/main/java/com/ironcoder/participantcenter/FriendsMapActivity.java
@@ -1,21 +1,57 @@
 package com.ironcoder.participantcenter;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 
 
-public class FriendsMapActivity extends FragmentActivity {
+public class FriendsMapActivity extends FragmentActivity implements GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
 
-    GoogleMap mMap;
-    SupportMapFragment mapFrag;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.map_activity);
 
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                mMap.setOnInfoWindowClickListener(this);
+                mMap.setInfoWindowAdapter(this);
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        return;
     }
 }
